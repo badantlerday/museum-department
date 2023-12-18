3badea716f17f42a585ee623a954714a0b8c5d8d
Johan Mårtensson <johan@popfrilla.nu>
2023-12-18 22:25:59 +0100

    fix char

--- components/BecomeASupporter.jsx
+++ components/BecomeASupporter.jsx
@@ -6,7 +6,7 @@ export default function BecomeASupporter() {
         <div className="article font-medium">
         <h2 className="mb-4 uppercase">Join us on building a Community</h2>
             <p>At Museum Department, we champion independent publishing to celebrate great design. Your support is vital in enabling us to an environment that thrives on creativity and a devotion to quality content.</p>
-    <p>By becoming a supporter, you're contributing to an (almost) ad-free platform for an uninterrupted experience that spotlights the best in design.</p>
+    <p>By becoming a supporter, youre contributing to an (almost) ad-free platform for an uninterrupted experience that spotlights the best in design.</p>
     <p>We have 2 tiers and if you sign up for a year you get 2 months discount (*saving 16,5%).</p>
     <p>Curator Tier — For $5 a month, you gain the ability to bookmark your favorite content, curating your personal Museum Department experience.</p>
     <p>Patron Tier — For $10 a month, you also get access to our exclusive design community on Discord. This space fosters connections with fellow creatives, fostering discussions, collaborations, and a  community of like-minded individuals sharing their passion for design.</p>
