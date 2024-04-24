4fd137ca1c4df3845998115a65a475891307e831
Johan Mårtensson <johan@popfrilla.nu>
2024-04-24 23:50:12 +0200

    Added footer logo

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -15,6 +15,7 @@ export const query = `*[_type == "settings"][0]{
 import { sanityFetch } from "@/lib/sanity.fetch"
 import CustomLink from "@/components/CustomLink"
 import Link from "next/link"
+import Image from "next/image"
 
 export default async function Footer() {
   const data = await sanityFetch({ query, tags: ["settings"] })
@@ -22,7 +23,14 @@ export default async function Footer() {
 
   return (
     <footer className="text-sm pt-80 pb-4 px-4 lg:px-20">
-			<div className="grid grid-cols-2 gap-y-8 lg:grid-cols-6 gap-4 w-full font-medium">
+			<Image
+				src="/museum-department.svg"
+				width={13}
+				height={13}
+				className="w-full h-auto"
+				alt="Cover"
+			/>
+			{/* <div className="grid grid-cols-2 gap-y-8 lg:grid-cols-6 gap-4 w-full font-medium">
 				<div className=" col-span-2 lg:col-span-2">
 					<h2 className="uppercase tracking-wide">Museum Department</h2>
 					<p>Curating Contemporary Culture</p>
@@ -67,7 +75,7 @@ export default async function Footer() {
 						<li>Pinterest</li>
 					</ul>
 				</div>
-			</div>
+			</div> */}
 			<div className="text-xs mt-28">
 				{" "}
 				© Museum Department 2024    All images © of their respective owners.
